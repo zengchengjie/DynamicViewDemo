b9cd14497495f76effb60de28f9f18b9ecaad63f
zengchengjie <zengchengjieit@163.com>
2018-01-22 20:42:18 +0800

    修改

--- app/src/main/java/com/example/a10062376/dynamicviewdemo/MainActivity.java
+++ app/src/main/java/com/example/a10062376/dynamicviewdemo/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.StringRes;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -18,13 +20,37 @@ public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
     List<PageModel> pageModels = new ArrayList<>();
 
+    {
+        //初始化界面
+        pageModels.add(new PageModel(R.string.drawShape, R.layout.fragment_shape));
+        pageModels.add(new PageModel(R.string.drawText, R.layout.fragment_text));
+        pageModels.add(new PageModel(R.string.drawBitmap, R.layout.fragment_bitmap));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pageModels.add(new PageModel(R.string.drawShape, R.layout.fragment_shape));
-        pageModels.add(new PageModel(R.string.drawText, R.layout.fragment_text));
-        pageModels.add(new PageModel(R.string.drawBitmap, R.layout.fragment_bitmap));
+        viewPager = findViewById(R.id.pager);
+        /*viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                PageModel pageModel = pageModels.get(position);
+
+                return pageF;
+            }
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                //根据给定的参数索引，检索指定参数的值作为 Java 编程语言中的 String。
+                return getString(pageModels.get(position).titleRes);
+            }
+        });*/
     }
 
     private class PageModel {
@@ -39,4 +65,5 @@ public class MainActivity extends AppCompatActivity {
             this.practiceLayout = practiceLayout;
         }
     }
+
 }
---
+++ app/src/main/java/com/example/a10062376/dynamicviewdemo/PageFragment.java
@@ -0,0 +1,17 @@
+package com.example.a10062376.dynamicviewdemo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+/**
+ * Created by 10062376 on 2018/1/18.
+ */
+
+public class PageFragment extends Fragment {
+    public static PageFragment newIncetance() {
+        PageFragment pageFragment = new PageFragment();
+        Bundle args = new Bundle();
+
+        return pageFragment;
+    }
+}
